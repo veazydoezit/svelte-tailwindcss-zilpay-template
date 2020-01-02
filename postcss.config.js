2e6ae7d04eaf95fbdac0defe73f1fa4af1e5c275
Mehmet Sarıoğlu <mehmet.sarioglu@dcipheranalytics.com>
2020-01-02 18:13:18 +0300

    Update purgecss config to have broader coverage

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,12 @@
 const purgecss = require('@fullhuman/postcss-purgecss')({
   content: [
-    './src/**/*.html',
-    './src/**/*.svelte'
+    './**/**/*.html',
+    './**/**/*.svelte'
   ],
-  
+
   whitelistPatterns: [/svelte-/],
 
-  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
 });
 
 const production = !process.env.ROLLUP_WATCH
