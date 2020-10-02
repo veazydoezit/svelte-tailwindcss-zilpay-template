0d25dcdd68784f37ca366c29b03e3067a2cea00f
Julian Groen <43829428+juliangroen@users.noreply.github.com>
2020-10-02 02:33:09 -0600

    Fix Tailwind Purge for Svelte Class Directives (#2)
    * new default extractor for svelte class directive
    
    found via https://github.com/babichjacob/sapper-postcss-template
    
    * included autoprefixer postcss plugin
    
    * added autoprefixer to plugins
    
    * removed autoprefixer plugin, commented out tailwindcss future features

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,22 @@
 module.exports = {
-  purge: {
-    mode: 'all',
-    content: ['./**/**/*.html', './**/**/*.svelte'],
+    purge: {
+        mode: 'all',
+        content: ['./**/**/*.html', './**/**/*.svelte'],
 
-    options: {
-      whitelistPatterns: [/svelte-/],
+        options: {
+            whitelistPatterns: [/svelte-/],
+            defaultExtractor: (content) =>
+                [...content.matchAll(/(?:class:)*([\w\d-/:%.]+)/gm)].map(([_match, group, ..._rest]) => group),
+        },
     },
-  },
 
-  theme: {
-    extend: {},
-  },
-  variants: {},
-  plugins: [],
+    theme: {
+        extend: {},
+    },
+    variants: {},
+    plugins: [],
+    future: {
+        // purgeLayersByDefault: true,
+        // removeDeprecatedGapUtilities: true,
+    },
 };
